32b3bb6191dc693d630d926090fd5a55dc352ff9
ദ്ദി˶𖥦)🍋 <rhyme.qing@gmail.com>
2025-05-06 22:52:04 +0800

    feat: add metals

--- lua/plugins/code/conform.lua
+++ lua/plugins/code/conform.lua
@@ -1,4 +1,29 @@
 return {
+  {
+    "scalameta/nvim-metals",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    ft = { "scala", "sbt", "java" },
+    opts = function()
+      local metals_config = require("metals").bare_config()
+      metals_config.on_attach = function(client, bufnr)
+
+      end
+
+      return metals_config
+    end,
+    config = function(self, metals_config)
+      local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = self.ft,
+        callback = function()
+          require("metals").initialize_or_attach(metals_config)
+        end,
+        group = nvim_metals_group,
+      })
+    end,
+  },
   "stevearc/conform.nvim",
   opts = {
     formatters_by_ft = {
@@ -9,7 +34,7 @@ return {
         command = "scalafmt",
         args = { "--stdin" },
         stdin = true,
-        cwd = require("conform.util").root_file({ ".scalafmt.conf", "build.sbt", ".git" }),  
+        cwd = require("conform.util").root_file({ ".scalafmt.conf", "build.sbt", ".git" }),
       },
     },
   },
