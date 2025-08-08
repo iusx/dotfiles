dd237d367586f282063ce48266546c5703689909
ദ്ദി˶𖥦)ദ്ദി <rhyme.qing@gmail.com>
2025-08-09 01:35:48 +0800

    feat: add new config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,5 +1,21 @@
+--[[ EDIT ]]
 vim.opt.clipboard = "unnamedplus"
 local opt = vim.opt
 opt.spelllang = { "en", "cjk" }
 vim.opt.wrap = true
 vim.g.autoformat = false
+
+
+--[[ UI ]]
+vim.opt.number = true
+vim.opt.signcolumn = "yes"
+vim.opt.cursorline = true
+
+--[[ Code ]]
+vim.opt.tabstop = 2      
+vim.opt.shiftwidth = 2   
+vim.opt.expandtab = true 
+
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldlevel = 99
--- lua/plugins/code/conform.lua
+++ lua/plugins/code/conform.lua
@@ -1,70 +1,83 @@
 return {
-  --[[ { ]]
-  --[[   "scalameta/nvim-metals", ]]
-  --[[   dependencies = { ]]
-  --[[     "nvim-lua/plenary.nvim", ]]
-  --[[   }, ]]
-  --[[   ft = { "scala", "sbt", "java" }, ]]
-  --[[   opts = function() ]]
-  --[[     local metals_config = require("metals").bare_config() ]]
-  --[[     metals_config.on_attach = function(client, bufnr) end ]]
-  --[[]]
-  --[[     return metals_config ]]
-  --[[   end, ]]
-  --[[   config = function(self, metals_config) ]]
-  --[[     local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true }) ]]
-  --[[     vim.api.nvim_create_autocmd("FileType", { ]]
-  --[[       pattern = self.ft, ]]
-  --[[       callback = function() ]]
-  --[[         require("metals").initialize_or_attach(metals_config) ]]
-  --[[       end, ]]
-  --[[       group = nvim_metals_group, ]]
-  --[[     }) ]]
-  --[[   end, ]]
-  --[[ }, ]]
-  --[[ "stevearc/conform.nvim", ]]
-  --[[ opts = { ]]
-  --[[   formatters_by_ft = { ]]
-  --[[     css = { "prettier" }, ]]
-  --[[     scala = { "scalafmt" }, ]]
-  --[[     avascript = { "prettier" }, ]]
-  --[[     typescript = { "prettier" }, ]]
-  --[[     javascriptreact = { "prettier" }, ]]
-  --[[     typescriptreact = { "prettier" }, ]]
-  --[[     html = { "prettier" }, ]]
-  --[[     json = { "prettier" }, ]]
-  --[[     yaml = { "prettier" }, ]]
-  --[[     markdown = { "prettier" }, ]]
-  --[[     graphql = { "prettier" }, ]]
-  --[[   }, ]]
-  --[[   formatters = { ]]
-  --[[     scalafmt = { ]]
-  --[[       command = "scalafmt", ]]
-  --[[       args = { "--stdin" }, ]]
-  --[[       stdin = true, ]]
-  --[[       cwd = require("conform.util").root_file({ ".scalafmt.conf", "build.sbt", ".git" }), ]]
-  --[[     }, ]]
-  --[[   }, ]]
-  --[[ }, ]]
-  -- ~/.config/nvim/lua/plugins/conform.lua
-  "stevearc/conform.nvim",
-  opts = {
-    formatters_by_ft = {
-      css = { "prettier" },
-      javascript = { "prettier" },
-      typescript = { "prettier" },
-      javascriptreact = { "prettier" },
-      typescriptreact = { "prettier" },
-      json = { "prettier" },
-      yaml = { "prettier" },
-      vue = { "prettier" },
-    },
-    formatters = {
-      prettier = {
-        command = "prettier",
-        args = { "--stdin-filepath", "$FILENAME" },
-        stdin = true,
-      },
-    },
-  },
+	--[[ { ]]
+	--[[   "scalameta/nvim-metals", ]]
+	--[[   dependencies = { ]]
+	--[[     "nvim-lua/plenary.nvim", ]]
+	--[[   }, ]]
+	--[[   ft = { "scala", "sbt", "java" }, ]]
+	--[[   opts = function() ]]
+	--[[     local metals_config = require("metals").bare_config() ]]
+	--[[     metals_config.on_attach = function(client, bufnr) end ]]
+	--[[]]
+	--[[     return metals_config ]]
+	--[[   end, ]]
+	--[[   config = function(self, metals_config) ]]
+	--[[     local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true }) ]]
+	--[[     vim.api.nvim_create_autocmd("FileType", { ]]
+	--[[       pattern = self.ft, ]]
+	--[[       callback = function() ]]
+	--[[         require("metals").initialize_or_attach(metals_config) ]]
+	--[[       end, ]]
+	--[[       group = nvim_metals_group, ]]
+	--[[     }) ]]
+	--[[   end, ]]
+	--[[ }, ]]
+	--[[ "stevearc/conform.nvim", ]]
+	--[[ opts = { ]]
+	--[[   formatters_by_ft = { ]]
+	--[[     css = { "prettier" }, ]]
+	--[[     scala = { "scalafmt" }, ]]
+	--[[     avascript = { "prettier" }, ]]
+	--[[     typescript = { "prettier" }, ]]
+	--[[     javascriptreact = { "prettier" }, ]]
+	--[[     typescriptreact = { "prettier" }, ]]
+	--[[     html = { "prettier" }, ]]
+	--[[     json = { "prettier" }, ]]
+	--[[     yaml = { "prettier" }, ]]
+	--[[     markdown = { "prettier" }, ]]
+	--[[     graphql = { "prettier" }, ]]
+	--[[   }, ]]
+	--[[   formatters = { ]]
+	--[[     scalafmt = { ]]
+	--[[       command = "scalafmt", ]]
+	--[[       args = { "--stdin" }, ]]
+	--[[       stdin = true, ]]
+	--[[       cwd = require("conform.util").root_file({ ".scalafmt.conf", "build.sbt", ".git" }), ]]
+	--[[     }, ]]
+	--[[   }, ]]
+	--[[ }, ]]
+	"stevearc/conform.nvim",
+	opts = function()
+		local opts = {
+			formatters_by_ft = {
+				css = { "prettier" },
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				javascriptreact = { "prettier" },
+				typescriptreact = { "prettier" },
+				json = { "prettier" },
+				yaml = { "prettier" },
+				vue = { "prettier" },
+				lua = { "stylua" },
+			},
+			formatters = {
+				prettier = {
+					command = "prettier",
+					args = { "--stdin-filepath", "$FILENAME" },
+					stdin = true,
+				},
+				stylua = {
+					command = "stylua",
+					args = { "--search-parent-directories", "--stdin-filepath", "$FILENAME", "-" },
+					stdin = true,
+				},
+			},
+		}
+
+		vim.keymap.set("n", "<leader>cf", function()
+			require("conform").format({ async = true, lsp_fallback = true })
+		end, { desc = "Format file" })
+
+		return opts
+	end,
 }
