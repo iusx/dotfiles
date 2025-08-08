34e0325514b7f213032788159bec18957a7f6920
ദ്ദി˶𖥦)ദ്ദി <rhyme.qing@gmail.com>
2025-08-09 02:39:02 +0800

    feat: add new config

--- lua/plugins/ui/bufferline.lua
+++ lua/plugins/ui/bufferline.lua
@@ -1,8 +1,27 @@
 return {
-  "akinsho/bufferline.nvim",
-  config = function()
-    require("bufferline").setup {}
-    vim.keymap.set("n", "<S-l>", "<cmd>BufferLineCycleNext<CR>", { desc = "> buffer" })
-    vim.keymap.set("n", "<S-j>", "<cmd>BufferLineCyclePrev<CR>", { desc = "< buffer" })
-  end,
+	"akinsho/bufferline.nvim",
+	config = function()
+		require("bufferline").setup({})
+		vim.keymap.set(
+			"n",
+			"<S-l>",
+			"<cmd>BufferLineCycleNext<CR>",
+			{ desc = "> buffer" }
+		)
+		vim.keymap.set(
+			"n",
+			"<S-j>",
+			"<cmd>BufferLineCyclePrev<CR>",
+			{ desc = "< buffer" }
+		)
+
+		vim.keymap.set(
+			"n",
+			"<S-k>",
+			"<cmd>BufferLinePickClose<CR>",
+			{
+				desc = "Pick & close buffer",
+			}
+		)
+	end,
 }
