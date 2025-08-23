eb5bb7930884d4dde06649d42eeb1f9d83c80e50
ദ്ദി˶𖥦)ദ്ദി <rhyme.qing@gmail.com>
2025-08-23 15:06:30 +0800

    style: use directory

--- lua/plugins/code/autotag.lua
+++
@@ -1,6 +0,0 @@
-return {
-  'windwp/nvim-ts-autotag',
-  event = { 'BufReadPost', 'BufNewFile', 'BufWritePre' },
-  ft = { 'html', 'javascriptreact', 'typescriptreact' },
-  opts = {},
-}
--- lua/plugins/init.lua
+++
@@ -1,6 +0,0 @@
-return {
-  { import = "plugins.ui" },
-  { import = "plugins.code" },
-  { import = "plugins.editor" },
-  --[[ { import = "plugins.lang" }, ]]
-}
